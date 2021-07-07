9d25fd37b8d928267f6ad3201c7344b8733f0f38
Sagar Adhikari <sageradh@gmail.com>
2021-07-08 00:47:30 +0545

    Way to ssh all

--- ssh-server.sh
+++ ssh-server.sh
@@ -1,3 +1,21 @@
 # ssh asmi@124.41.198.206
 ssh -L 5555:localhost:1234 asmi@124.41.198.206
 password: asmi123
+
+
+
+#go to ssh folder
+ssh -i manishb  -L 1234:localhost:1234 manishb@124.41.198.206
+ssh -i nitenl   -L 1235:localhost:1235 nitenl@124.41.198.206
+ssh -i gaurabs  -L 1236:localhost:1236 gaurabs@124.41.198.206
+ssh -i subashs  -L 1237:localhost:1237 subashs@124.41.198.206
+ssh -i bijayd   -L 1238:localhost:1238 bijayd@124.41.198.206
+ssh -i roshans  -L 1239:localhost:1239 roshans@124.41.198.206
+ssh -i prakashd -L 1240:localhost:1240 prakashd@124.41.198.206
+ssh -i bigyans  -L 1241:localhost:1241 bigyans@124.41.198.206
+ssh -i sagara   -L 1242:localhost:1242 sagara@124.41.198.206
+ssh -i rkl      -L 1243:localhost:1243 rkl@124.41.198.206
+ssh -i jabirk   -L 1244:localhost:1244 jabirk@124.41.198.206
+ssh -i raghibi  -L 1245:localhost:1245 raghibi@124.41.198.206
+ssh -i ashishs  -L 1246:localhost:1246 ashishs@124.41.198.206
+
